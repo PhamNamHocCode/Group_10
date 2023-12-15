c46ef36ae9029c944c09b0c490763420453c96f1
Albes0701 <quyhung0701@gmail.com>
2023-12-15 22:57:03 +0700

    Add

--- client/js/filter.js
+++ client/js/filter.js
@@ -171,7 +171,7 @@ let decrement = (id) => {
   
     // update(selectedItem.id);
     basket = basket.filter((x) => x.item !== 0);
-    console.log(basket);
+    //console.log(basket);
     localStorage.setItem("data", JSON.stringify(basket));
 };
 
